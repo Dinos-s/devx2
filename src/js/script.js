21c9d1d73d63369e2a346ed9696ae47fa0f508ce
Gabriel <gabriel02ramos@gmail.com>
2022-01-16 17:01:00 -0300

    Um pequeno upgrade
    Tentativa de colocar a opacidade nas setas avançar e voltar.

--- src/js/script.js
+++ src/js/script.js
@@ -10,12 +10,17 @@ function HiddenImages() {
 }
 
 function ShowImages() {
-    imagens[imagemAtual].classList.add('mostrar');
+    (imagens[imagemAtual].classList.add('mostrar'));
 }
 
 setaAvancar.addEventListener('click', () => {
     if(imagemAtual === (imagens.length - 1)){
         console.log('acabou as imagens');
+        if (imagemAtual != -1) {
+            setaAvancar.style.opacity = .5;
+            setaVoltar.style.opacity = 1;
+        }
+        // Se a imagem atual for diferente de 2 (no caso seria a ultima imagem apresentada), se clicado mais uma vez ele ficará opaco enquanto o botão voltar ficará mais claro;
         return;
     }
 
@@ -28,6 +33,11 @@ setaAvancar.addEventListener('click', () => {
 setaVoltar.addEventListener('click', () => {
     if (imagemAtual === 0) {
         console.log('acabou as imagens');
+        if (imagemAtual != 1) {
+            setaVoltar.style.opacity = .5;
+            setaAvancar.style.opacity = 1;
+        }
+        // Se a imagem atual for diferente de -1 (no caso seria a imagem apresentada logo de cara na page), se clicado mais uma vez ele ficará opaco enquanto o botão avançar ficará mais claro;
         return;
     }
     imagemAtual--;
